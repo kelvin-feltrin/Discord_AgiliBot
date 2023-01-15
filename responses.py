1cc0c3941f474b397ec0d858a3f24366dd730647
kelvin-feltrin <kelvin-798@hotmail.com>
2023-01-15 20:26:28 -0300

    Criacao de bot para o Discord

---
+++ responses.py
@@ -0,0 +1,21 @@
+def get_response(message: str) -> str:
+    p_message = message.lower()
+
+    if p_message == '!hello':
+        return 'Hello world!'
+
+    if p_message.startswith('!livro'):
+        text_params = message.replace('!livro', '')
+        params_list = text_params.split(';')
+
+        if len(params_list) == 4:
+            title = params_list[0]
+            author = params_list[1]
+            category = params_list[2]
+            link = params_list[3]
+
+            message_formatted = f'📚 | **Título:** {title} \n\n✍️ | **Autor:** {author} \n\n📌 | **Categoria:** {category} \n\n🔗 | **Link:** {link}'
+
+            return message_formatted
+        else:
+            return 'Comando inválido! Digite `!livro titulo; autor; categoria; link`'
